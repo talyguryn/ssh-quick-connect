bd335209c3d0aa946b290189f21ed394cf98b854
Taly <vitalik7tv@yandex.ru>
2020-09-22 21:00:41 +0300

    use oascript to open terminal

--- src/actions/session.js
+++ src/actions/session.js
@@ -1,23 +1,7 @@
 const { exec } = require('child_process');
 
 const session = function (command) {
-    const scriptName = Math.random().toString(36).substr(2, 9),
-        scriptPath = `${require('temp-dir')}/${scriptName}`,
-        scriptContent = `#! /bin/bash\n` +
-                        `\n` +
-                        `${command}`;
-
-    exec(`echo "${scriptContent}" > ${scriptPath}`);
-    exec(`chmod +x ${scriptPath}`);
-    exec(`open ${scriptPath}`, (code, stdout, stderr) => {
-        /**
-         * Check if command execution was failed
-         */
-        if (code !== 0) {
-            global.logger.error(stderr);
-            return;
-        }
-    });
+    exec(`osascript -e 'tell application "Terminal" to (do script "${command}") activate'`);
 }
 
 module.exports = session;
