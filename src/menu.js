d2537023c9e7ad9beedaf00ca15f746d0d756d02
Taly <vitalik7tv@yandex.ru>
2021-07-07 22:48:50 +0300

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -8,7 +8,7 @@ const path = require('path');
 const checkInternetConnected = require('check-internet-connected');
 
 const NAME = 'SSH Quick Connect';
-const VERSION = '1.0.5';
+const VERSION = '1.0.6';
 
 const checkSite = require('./utils/checkSite');
 
